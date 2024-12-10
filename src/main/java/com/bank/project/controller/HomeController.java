8934f3204db527647308f95d08e3c8ffd867f060
Nangzu <126366721+Nangzu@users.noreply.github.com>
2024-12-10 13:29:19 +0900

    뭔가됐음

--- src/main/java/com/bank/project/controller/HomeController.java
+++ src/main/java/com/bank/project/controller/HomeController.java
@@ -2,10 +2,11 @@ package com.bank.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index"; // => index.jsp 출력
     }
