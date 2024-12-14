c6d42642dbe37ad67419fd1e33848edcf6bc57ec
parkgw2000 <148188690+parkgw2000@users.noreply.github.com>
2024-12-14 17:45:17 +0900

    first
    first

---
+++ stsproject/src/main/java/com/example/stsproject/StsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.stsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StsprojectApplication.class, args);
+    }
+
+}
